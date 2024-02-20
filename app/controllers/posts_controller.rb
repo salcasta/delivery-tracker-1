fa7de47e05f8d90cd2bcc5eeff59d442d24f73c0
Salvador Castaneda <57605337+salcasta@users.noreply.github.com>
2024-02-20 02:33:00 +0000

    lib dev.rake fixed to allow sample_data to run. Minor typo fixed, all tests passed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,9 @@ class PostsController < ApplicationController
   def index
     matching_posts = Post.all
 
-    @list_of_posts = matching_posts.order({ :created_at => :desc })
+    @list_of_posts = matching_posts.order({ :arrive_on => :asc }).where({ :arrived => false}).where({ :user_id => current_user.id })
+
+    @list_of_posts_received = matching_posts.order({ :update_at => :desc }).where({ :arrived => true }).where({ :user_id => current_user.id })
 
     render({ :template => "posts/index" })
   end
@@ -23,7 +25,7 @@ class PostsController < ApplicationController
     the_post.arrive_on = params.fetch("query_arrive_on")
     the_post.detail = params.fetch("query_detail")
     the_post.user_id = params.fetch("query_user_id")
-    the_post.arrived = params.fetch("query_arrived")
+    the_post.arrived = false
 
     if the_post.valid?
       the_post.save
@@ -37,16 +39,13 @@ class PostsController < ApplicationController
     the_id = params.fetch("path_id")
     the_post = Post.where({ :id => the_id }).at(0)
 
-    the_post.description = params.fetch("query_description")
-    the_post.arrive_on = params.fetch("query_arrive_on")
-    the_post.detail = params.fetch("query_detail")
-    the_post.arrived = params.fetch("query_arrived")
+    the_post.arrived = true
 
     if the_post.valid?
       the_post.save
-      redirect_to("/posts/#{the_post.id}", { :notice => "Marked as received."} )
+      redirect_to("/posts/", { :notice => "Marked as received."} )
     else
-      redirect_to("/posts/#{the_post.id}", { :alert => the_post.errors.full_messages.to_sentence })
+      redirect_to("/posts/", { :alert => the_post.errors.full_messages.to_sentence })
     end
   end
 
--- lib/tasks/dev.rake
+++ lib/tasks/dev.rake
@@ -1,7 +1,7 @@
 desc "Hydrate the database with some sample data to look at so that developing is easier"
 task({ :sample_data => :environment}) do
   User.destroy_all
-  Delivery.destroy_all
+  Post.destroy_all
 
   usernames = ["alice", "bob", "carol", "dave", "eve"]
 
@@ -12,19 +12,19 @@ task({ :sample_data => :environment}) do
     user.save
 
     10.times do
-      delivery = Delivery.new
-      delivery.user_id = user.id
-      delivery.description = Faker::Commerce.product_name
-      delivery.details = "#{["FedEx", "UPS", "USPS"].sample} tracking ##{rand(1000000000000)}" if rand < 0.5
-      delivery.supposed_to_arrive_on = Faker::Date.between(from: 1.month.ago, to: 2.weeks.from_now)
+      post = Post.new
+      post.user_id = user.id
+      post.description = Faker::Commerce.product_name
+      post.detail = "#{["FedEx", "UPS", "USPS"].sample} tracking ##{rand(1000000000000)}" if rand < 0.5
+      post.arrive_on = Faker::Date.between(from: 1.month.ago, to: 2.weeks.from_now)
 
-      if delivery.supposed_to_arrive_on < Time.now
-        delivery.arrived = [true, false].sample
+      if post.arrive_on < Time.now
+        post.arrived = [true, false].sample
       else
-        delivery.arrived = false
+        post.arrived = false
       end
 
-      delivery.save
+      post.save
     end
   end
 end
